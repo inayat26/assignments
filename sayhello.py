363386ef6252e5bc0c883c3513db6192702b11b5
inayat26 <inayat_sabhikhi@student.hks.harvard.edu>
2020-01-10 10:59:29 -0500

    minor change

--- sayhello.py
+++ sayhello.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 import sys
+# hello, sample testing comment 
+#reading STDIN
 name = sys.stdin.read()
+#printing a greeting based on the input of usernmame
 print("Hello " + name + "!")
+# now lets see what's going to happen! 
+
+# make an additional comment
