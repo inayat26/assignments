27fe3dfbdf072468ff31e2477bbf76674e1c9fd3
inayat26 <inayat_sabhikhi@student.hks.harvard.edu>
2020-01-13 10:02:42 -0500

    adding files

---
+++ calculator.js
@@ -0,0 +1,21 @@
+// Define a Function
+function add() {
+	var a,b;
+	return a + b;
+}
+function subtract() {
+	var a,b;
+	return a - b;
+}
+	function multiply() {
+	var a,b;
+	return a * b;
+}
+	function divide() {
+	var a,b;
+	return a / b;
+}
+
+	console.log("I'm going use the calculator functions to multiply 5 and 6")
+var x = multiply(5,6)
+console.log(x)
